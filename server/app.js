8241048f4196e63094f7551bf26c29792bc9727b
Martín Farias <martin.akg1991@gmail.com>
2019-11-04 10:02:07 -0300

    api rest base

--- server/app.js
+++ server/app.js
@@ -35,9 +35,54 @@ const port      = 3035;
 http.createServer(function (req, res) {
     // .. Here you can create your data response in a JSON format
     
-    
-    res.write("Response goes in here..."); // Write out the default response
-    res.end(); //end the response
+    if(req.url.match(/^\/api\//)){
+        const urlData = req.url.split('?'); // 0: url, 1: querystring
+        const url = urlData[0];
+        var querystring = {}
+        if(urlData[1]){
+            const paramvalues = urlData[1].split('&');
+            for(var k in paramvalues){
+                var paramvalue = paramvalues[k].split('=');
+                querystring[paramvalue[0]] = paramvalue[1];
+            }
+        }
+        switch(url.substring(5)){
+            case 'products/all':
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(data));
+            break;
+            case 'products':
+                res.writeHead(200, {"Content-Type": "application/json"});
+                var rd = data;
+                if(querystring.query){
+                    rd = data.filter(o => ~o.name.toLowerCase().indexOf(querystring.query.toLowerCase()));
+                }
+                res.end(JSON.stringify(rd));
+            break;
+            case 'products/preview':
+                res.writeHead(200, {"Content-Type": "application/json"});
+                if(querystring.query){
+                    var rd = data;
+                    rd = data.filter(o => ~o.name.toLowerCase().indexOf(querystring.query.toLowerCase()));
+                    var fullinfo = {
+                        length: rd.length,
+                        data: rd.length > 4 ? rd.slice(1,5) : rd
+                    }
+                    res.end(JSON.stringify(fullinfo));
+                } else {
+                    res.end();
+                }
+                
+            break;
+            default:
+                res.write("Nothing matches at " + url); // Write out the default response
+                res.end(); //end the response
+        }
+        console.log(req.url);
+    } else {
+        res.write("Response goes in here..."); // Write out the default response
+        res.end(); //end the response
+    }
 }).listen( port );
 
 
